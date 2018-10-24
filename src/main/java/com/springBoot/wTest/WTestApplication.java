6732d810ff42004799e312025a34ae7bf8c7ef39
pangzi-git <whf786634141@sina.com>
2018-10-24 14:25:40 +0800

    version1

---
+++ src/main/java/com/springBoot/wTest/WTestApplication.java
@@ -0,0 +1,12 @@
+package com.springBoot.wTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/springBoot/wTest/WTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springBoot.wTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
